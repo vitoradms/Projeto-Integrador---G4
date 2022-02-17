e8d2eb55abf2a493bdedd26d26c98dff58339309
Adamis <vitoradms@hotmail.com>
2022-02-17 19:50:53 -0300

    ajuste de página home - link de ingrediente

--- routes/ingredientes.js
+++ routes/ingredientes.js
@@ -2,7 +2,7 @@ var ingredientesController = require("../controllers/ingredientesController")
 var express = require('express');
 var router = express.Router();
 
-router.post('/', ingredientesController.cadastrarIngrediente);
-router.get('/', ingredientesController.buscarIngrediente);
+router.post('/cadastraringrediente', ingredientesController.cadastrarIngrediente);
+router.get('/buscaringrediente', ingredientesController.buscarIngrediente);
 
 module.exports = router;
