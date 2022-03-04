0c9e8fbff6e5004e7d3bc38a6d836a815b9f49b1
JefersonPmatos <jeffpmatos@gmail.com>
2022-03-04 10:03:57 -0300

    cadastro usuário funcionando perfeitamente. Senha criptografada com hash.

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,21 +1,24 @@
-const usuario = require('../model/Usuario')
+const Usuario = require('../model/Usuario')
 
 const usuarioControllers = {
-  showCadastro: (req, res) => {
+  showCadastro(req, res) {
     return res.render('cadastro')
   },
-  deletar: (req, res) => {
+
+  async salvarUsuario(req, res){
+    const { nome, sobrenome, data_nascimento, email, senha }= req.body;
+    await Usuario.salvar(nome, sobrenome, data_nascimento, email, senha);
+    res.send("Salvo com sucesso")
+
+  },
+
+  deletar(req, res){
     return res.send('deletando usuario')
   },
-  atualizarDados: (req, res) => {
+
+  atualizarDados(req, res){
     return res.send('atualizando dados usuarios')
   },
-  salvarUsuario: async (req, res) => {
-    const { nome, email, senha } = req.body
-
-    const novoUsuario = await usuario.salvar(nome, email, senha);
-    res.send(novoUsuario)
-  }
 }
 
 module.exports = usuarioControllers
--- model/Usuario.js
+++ model/Usuario.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const { v4 } = require('uuid');
+const bcrypt = require('bcrypt');
 
 let db = require('../database/db.json');
-
-const { uploadPath } = require('../config/multer');
 
 const writeToDB = async () => {
     const json = JSON.stringify(db);
@@ -11,11 +10,14 @@ const writeToDB = async () => {
 };
 
 const Usuario =  {
-    async salvar (nome, email, senha) {
-        const cadastro = { id: v4(), nome, email, senha,}
-        db.usuarios.push(cadastro);
+    async salvar(nome, sobrenome, data_nascimento, email, senha) {
+        const saltRounds = 10
+        const hash = bcrypt.hashSync(senha, saltRounds)
+
+        const novoUsuario = { id: v4(), nome, sobrenome, data_nascimento, email, senha:hash, admin:false };
+        db.usuarios.push(novoUsuario);
         await writeToDB();
-        return cadastro
+        return novoUsuario
     }
 }
 
