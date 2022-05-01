c466542fa3a03ac74d4f9f838b4dd5095bc8cb7e
andisandis-lima <anderson.ls1998@gmail.com>
2022-05-01 09:32:23 -0300

    botao ocultar/mostrar info comecou a ser implementado

---
+++ public/scripts/categorias-receitas.js
@@ -0,0 +1,13 @@
+let ingredientes = document.getElementsByClassName("hidden");
+
+ingredientes.classList.toggle("hidden")
+
+// function mostra(id) {
+//     if (document.getElementById(id).style.display == 'block') {
+//         document.getElementById(id).style.display == 'none'
+//         document.getElementById(ing).value = "Mostrar"
+//     } else {
+//         document.getElementById(id).style.display == 'block'
+//         document.getElementById(ing).value = "Ocultar"
+//     }
+// }
