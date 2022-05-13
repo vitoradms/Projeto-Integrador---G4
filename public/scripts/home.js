7580e2148ebc41b3e973864ea75f95427beab4dd
JefersonPmatos <jeffpmatos@gmail.com>
2022-05-13 16:34:53 -0300

    css página home ajustado para corresponder com visual do site. Adicionado input dinamico para busca de receita

---
+++ public/scripts/home.js
@@ -0,0 +1,27 @@
+
+// Inputs dinâmicos
+let inputs = document.getElementById("inputs");
+let addInput = document.getElementById("addInput");
+let removeInput = document.getElementById("removeInput");
+
+addInput.onclick = function () {
+  let newInput = document.createElement("input");
+  newInput.setAttribute("type", "text");
+  newInput.setAttribute("name", "buscaIngrediente[]");
+  newInput.setAttribute("class", "input");
+  newInput.setAttribute("placeholder", "ADICIONE UM INGREDIENTE");
+
+  inputs.appendChild(newInput);
+
+};
+
+removeInput.onclick = function () {
+  if(inputs.children.length > 1) {
+      inputs.removeChild(inputs.lastChild)
+  }
+};
+
+// Validação campo nome receita
+
+let nomeReceita = document.getElementsByName('nome_receita');
+
