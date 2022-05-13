0b29c988781728ab70b466313040dcf68c27a7c3
JefersonPmatos <jeffpmatos@gmail.com>
2022-05-13 12:54:27 -0300

    mais css

--- public/scripts/salvar-receita.js
+++ public/scripts/salvar-receita.js
@@ -1,3 +1,5 @@
+
+// Inputs dinâmicos
 let ingredientes = document.getElementById("ingredientes");
 let addInput = document.getElementById("addInput");
 let removeInput = document.getElementById("removeInput");
@@ -10,7 +12,12 @@ addInput.onclick = function () {
 };
 
 removeInput.onclick = function () {
-  if(ingredientes.children.length > 2) {
+  if(ingredientes.children.length > 1) {
       ingredientes.removeChild(ingredientes.lastChild)
   }
 };
+
+// Validação campo nome receita
+
+let nomeReceita = document.getElementsByName('nome_receita');
+
